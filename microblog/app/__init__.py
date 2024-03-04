8630d6b3306d5a90f58fdf6b8f5ea7371b0af748
Ksenia Klokova <kseniaklokova@gmail.com>
2024-03-04 22:21:11 +0300

    семинар 10

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,6 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your secret key'
 
-from app import routes
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app import routes, models
