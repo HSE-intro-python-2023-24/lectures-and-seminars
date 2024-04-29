d3d31bc9493f6c1cc48a39eb1d624d22867b6ffe
Ksenia Klokova <kseniaklokova@gmail.com>
2024-04-29 23:19:29 +0300

    семинар 12

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -14,6 +14,8 @@ class User(UserMixin, db.Model):
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
     password_hash: so.Mapped[str] = so.mapped_column(sa.String(120))
+    about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
+    last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(default = lambda: datetime.now(timezone.utc))
     
     def set_password(self, password):
             self.password_hash = generate_password_hash(password)
